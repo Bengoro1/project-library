9546a24cdfd4ba021e9e9f7ab045403ac738722f
Benjamin <benjamin.barvas@gmail.com>
2023-08-11 18:28:31 +0200

    book constructor

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+  addToLibrary(this);
+}
